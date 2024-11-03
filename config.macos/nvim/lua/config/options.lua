895b97167fcaeac90927407965b13bb4e6145411
Ivo Costa Cunha <ivo.costacunha@icloud.com>
2024-11-03 14:36:51 +0000

    updated macos .zshrc

---
+++ config.macos/nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Tab config
+vim.opt.tabstop = 2                                                              
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- UI config
+vim.opt.number = true
+
+-- UI colors
+vim.opt.termguicolors = true
